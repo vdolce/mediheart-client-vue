bdf7c4fb7fd5c17a2adeace973fd177b6dadb09c
giuseppe <giuseppe.maddiona@gmail.com>
2021-03-17 20:21:02 +0100

    added axios request without username or pass

--- src/store/modules/loginStore/actions.js
+++ src/store/modules/loginStore/actions.js
@@ -1,40 +1,35 @@
-import axios from '../../../plugins/axios'
-import {router} from '../../../main'
-
+import axios from "../../../plugins/axios";
+import { router } from "../../../main";
 
 export default {
-    // login user
-    async loginUser({state, commit}){ 
-        var request = state.form
-        
-        await axios.post('/login/', request)
-            .then(response => {
-                
-                // go to personal-info
-                if(response.status == 200){
-                    console.log('Api login - POST')
-                    console.log(response)
+  // login user
+  async loginUser({ state, commit }) {
+    var request = state.form;
 
-                    // delete login and password
-                    commit('resetLoginForm')
+    if (!(request.username == "" || request.password == "")) {
 
-                    // store session token
-                    localStorage.setItem('token', response.data.token)
-                    localStorage.setItem('patientId', response.data.patientId)
-                    localStorage.setItem('email', response.data.username)
-                    console.log(localStorage)
+      await axios.post("/login/", request).then((response) => {
+        // go to personal-info
+        if (response.status == 200) {
+          console.log("Api login - POST");
+          console.log(response);
 
-                    // go to personal-info
-                    console.log('go to personal-info')
-                    router.push({name:'personal-info'})            
-                }
-                
+          // delete login and password
+          commit("resetLoginForm");
 
-                
-            })
-             
-        
-        
-    },
+          // store session token
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("patientId", response.data.patientId);
+          localStorage.setItem("email", response.data.username);
+          console.log(localStorage);
 
-}
+          // go to personal-info
+          console.log("go to personal-info");
+          router.push({ name: "personal-info" });
+        }
+      });
+
+    
+    }
+  },
+};
